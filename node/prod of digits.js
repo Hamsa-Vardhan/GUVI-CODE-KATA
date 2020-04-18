da2e63e308a2d4df71b9fd89cc6795da9326b161
Hamsa-Vardhan <hamsavardhan99@gmail.com>
2020-04-18 10:52:45 +0530

    Create prod of digits.js

---
+++ node/prod of digits.js
@@ -0,0 +1,8 @@
+const rl = require('readline');
+const inp = rl.createInterface({
+	input:process.stdin,
+	output:process.stdout
+});
+var n;
+inp.on('line',(line)=>{n = line.split('').map(v=>Number(v))});
+inp.on('close',()=>{console.log(n.reduce((a,b)=>a*b));});

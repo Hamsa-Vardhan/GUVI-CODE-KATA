6d2ebb188e13cca456e0657a2ab1732b319edcfc
Hamsa-Vardhan <hamsavardhan99@gmail.com>
2020-04-18 10:06:36 +0530

    Create min of two numbers.js

---
+++ node/min of two numbers.js
@@ -0,0 +1,12 @@
+const rl = require('readline');
+const inp = rl.createInterface({
+  input:process.stdin,
+  output:process.stdout
+});
+var lst;
+inp.on('line',(line)=>{
+  lst = line.split(' ').map(v => Number(v));
+});
+inp.on('close',()=>{
+  console.log(Math.max(...lst)-Math.min(...lst));
+});
